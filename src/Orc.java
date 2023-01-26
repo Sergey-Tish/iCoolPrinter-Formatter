77fc668d6eb612eaae07bf7d7444183f5803c0b3
Sergey Tishchenko <seti.volch@gmail.com>
2023-01-26 19:33:37 +0700

    add Playable and Orc

--- src/Orc.java
+++ src/Orc.java
@@ -1,2 +1,16 @@
-public class Orc {
+public class Orc implements Playable {
+    @Override
+    public String move() {
+        return "run";
+    }
+
+    @Override
+    public String attack() {
+        return "ax blow";
+    }
+
+    @Override
+    public String defence() {
+        return "block";
+    }
 }
