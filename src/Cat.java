c3d3fdf7e8bb90609a0ef29e3d428946b9d34f1a
Sergey Tishchenko <seti.volch@gmail.com>
2023-01-26 20:01:57 +0700

    add exercise 3 practica2

---
+++ src/Cat.java
@@ -0,0 +1,16 @@
+public class Cat implements Eat, Edible, Movable{
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void beEaten() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
---
+++ src/Mouse.java
@@ -0,0 +1,12 @@
+public class Mouse implements Edible, Movable{
+
+    @Override
+    public void beEaten() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+}
